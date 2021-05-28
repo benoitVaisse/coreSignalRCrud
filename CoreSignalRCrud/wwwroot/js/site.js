6ef9068a25a07c87d62a29023b6e802897340638
benoitVaisse <benoit.vaisse@gmail.com>
2021-05-28 16:09:55 +0200

    recuperation des product en ajax

--- CoreSignalRCrud/wwwroot/js/site.js
+++ CoreSignalRCrud/wwwroot/js/site.js
@@ -1,1 +1,35 @@
 ﻿
+
+$(document).ready(() => {
+
+    
+    loadProdData();
+    function loadProdData() {
+        let tr = "";
+        $.ajax({
+            method: "GET",
+            url: "/Products/GetProducts",
+            success: (result) => {
+                console.log(result);
+                for (index in result) {
+                    tr +=   `<tr>
+                                <td>${result[index].name}</td>
+                                <td>${result[index].category}</td>
+                                <td>${result[index].price}</td>
+                                <td>${result[index].stockQty}</td>
+                                <td>
+                                    <a href='../Products/Edit?id=${result[index].id}'>Edit</a>
+                                    <a href='../Products/Details?id=${result[index].id}'>Detail</a>
+
+                                </td>
+                            </tr>`;
+                }
+
+                $("#tableBody").html(tr);
+            },
+            error: (err) => {
+                console.log(error);
+            }
+        })
+    }
+})
