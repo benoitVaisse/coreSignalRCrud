1f518991ff220e9123492e25143f8483f7ffae99
benoitVaisse <benoit.vaisse@gmail.com>
2021-05-28 15:03:43 +0200

    add connection db

---
+++ CoreSignalRCrud/Models/Products.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreSignalRCrud.Models
+{
+    public class Products
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockQty { get; set; }
+    }
+}
